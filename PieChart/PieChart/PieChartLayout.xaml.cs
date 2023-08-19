9f88c62f6bacbeace5388a4681afaa0f9564ea5f
Martin13553 <82732771+Martin13553@users.noreply.github.com>
2023-08-19 13:29:49 +0300

    Add files via upload

---
+++ PieChart/PieChart/PieChartLayout.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dashboard
+{
+    public partial class PieChartLayout : UserControl
+    {
+        #region dependency properties
+
+        /// The property of the bound object that will be plotted (CLR wrapper)
+        public String PlottedProperty
+        {
+            get { return GetPlottedProperty(this); }
+            set { SetPlottedProperty(this, value); }
+        }
+
+        // PlottedProperty dependency property
+        public static readonly DependencyProperty PlottedPropertyProperty =
+                       DependencyProperty.RegisterAttached("PlottedProperty", typeof(String), typeof(PieChartLayout),
+                       new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.Inherits));
+
+        // PlottedProperty attached property accessors
+        public static void SetPlottedProperty(UIElement element, String value)
+        {
+            element.SetValue(PlottedPropertyProperty, value);
+        }
+        public static String GetPlottedProperty(UIElement element)
+        {
+            return (String)element.GetValue(PlottedPropertyProperty);
+        }
+
+        public IColorSelector ColorSelector
+        {
+            get { return GetColorSelector(this); }
+            set { SetColorSelector(this, value); }
+        }
+
+        // ColorSelector dependency property
+        public static readonly DependencyProperty ColorSelectorProperty =
+                       DependencyProperty.RegisterAttached("ColorSelectorProperty", typeof(IColorSelector), typeof(PieChartLayout),
+                       new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
+
+        // ColorSelector attached property accessors
+        public static void SetColorSelector(UIElement element, IColorSelector value)
+        {
+            element.SetValue(ColorSelectorProperty, value);
+        }
+        public static IColorSelector GetColorSelector(UIElement element)
+        {
+            return (IColorSelector)element.GetValue(ColorSelectorProperty);
+        }
+
+        #endregion
+        public PieChartLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestData/AssetClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ychet 
+{
+    public class AssetClass : INotifyPropertyChanged
+    {
+        private string category;
+        public string Category
+        {
+            get { return category; }
+            set
+            {
+                if(category == value)
+                    return;
+
+                category = value;
+                RaisePropertyChanged(nameof(Category));
+            }
+        }
+
+        private double cash;
+        public double Cash
+        {
+            get { return cash; }
+            set
+            {
+                cash = value;
+                RaisePropertyChanged("Cash");
+            }
+        }
+
+        private double card1;
+        public double Card1
+        {
+            get { return card1; }
+            set
+            {
+                card1 = value;
+                RaisePropertyChanged("Card1");
+            }
+        }
+
+        private double card2;
+
+        public double Card2
+        {
+            get { return card2; }
+            set
+            {
+                card2 = value;
+                RaisePropertyChanged("Card2");
+            }
+        }
+
+        public static List<AssetClass> ConstructTestData()
+        {
+            List<AssetClass> assetClasses = new List<AssetClass>();
+
+            assetClasses.Add(new AssetClass() { Category = "Food", Cash = 10001, Card1 = 500, Card2 = 400 });
+            assetClasses.Add(new AssetClass() { Category = "Auto", Cash = 0, Card1 = 400, Card2 = 170 });
+            assetClasses.Add(new AssetClass() { Category = "Mobile", Cash = 1300, Card1 = 200, Card2 = 1 });
+            assetClasses.Add(new AssetClass() { Category = "Cafe", Cash = 1600, Card1 = 1600, Card2 = 800 });
+            assetClasses.Add(new AssetClass() { Category = "Education", Cash = 270, Card1 = 27, Card2 = 38 });
+            assetClasses.Add(new AssetClass() { Category = "Flat", Cash = 5000, Card1 = 50, Card2 = 300 });
+
+            return assetClasses;
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
